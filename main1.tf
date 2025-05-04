0d0d13f4b0a67bb09ee2192de40da37cc2f1c6c4
AbhayBhadauriya15 <er.abhaybhadauriya@gmail.com>
2025-05-04 07:14:23 +0530

    added a storage account in main1.tf

--- main1.tf
+++ main1.tf
@@ -14,3 +14,11 @@ resource "azurerm_resource_group" "name" {
  name = "mango"
  location = "east us"
 }
+resource "azurerm_storage_account" "name" {
+  name                     = "eddystg"
+  resource_group_name      = azurerm_resource_group.name.name
+  location                 = azurerm_resource_group.name.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  
+}
